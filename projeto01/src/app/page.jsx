0eedbb6ca3082463bcb8ab672c61e5a58440cb11
onlyzs2 <nic123hello@gmail.com>
2026-08-05 17:10:29 -0300

    nextsons

---
+++ projeto01/src/app/page.jsx
@@ -0,0 +1,17 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <>
+    <nav className="nav"> <img src="https://cdn.prod.website-files.com/654419c1986c7805ce020a1c/65e2351d004b15254286c078_Tier11-2024-Branding-Logo-Main.svg"/> 
+    <a href="#">what we do</a><a href="#">BLOG</a><a href="#">PODCAST</a><a href="#">CARRERS</a><a href="#">WORK WITH US</a></nav>
+    <div className="content">
+            <div className="principal_text">
+              <h2>Unlock Your Business Potential With Facebook & Instagram Advertising</h2>
+              <p>Facebook Premier Level Partner Agency</p>
+            </div>
+    </div>
+    </>
+    );
+}
