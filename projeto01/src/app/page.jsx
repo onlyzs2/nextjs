2123536601d6daba3d133a535416ae055bcac22e
onlyzs2 <nic123hello@gmail.com>
2026-08-06 16:59:05 -0300

    att no site

--- projeto01/src/app/page.jsx
+++ projeto01/src/app/page.jsx
@@ -4,14 +4,35 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <>
-    <nav className="nav"> <img src="https://cdn.prod.website-files.com/654419c1986c7805ce020a1c/65e2351d004b15254286c078_Tier11-2024-Branding-Logo-Main.svg"/> 
-    <a href="#">what we do</a><a href="#">BLOG</a><a href="#">PODCAST</a><a href="#">CARRERS</a><a href="#">WORK WITH US</a></nav>
-    <div className="content">
-            <div className="principal_text">
-              <h2>Unlock Your Business Potential With Facebook & Instagram Advertising</h2>
-              <p>Facebook Premier Level Partner Agency</p>
-            </div>
-    </div>
+      <header>
+        <nav className="navbar">
+          <div className="logo"><img src="https://thumb.tildacdn.com/tild6562-3465-4362-b435-343935623062/-/format/webp/Frame_1908.png" alt="imagem" />
+          </div>
+          <div className="navRedirect">
+            <select>
+              <option>WHAT WE DO </option>
+            </select>
+            <a href="">BLOG</a>
+            <a href="">PODCAST</a>
+            <a href="">CARRERS</a>
+            <a className="wwu" href="">WORK WITH US</a>
+          </div>
+        </nav>
+      </header>
+      <div className="principal">
+        <div className="principalText">
+          <h2>Unlock Your Business Potential With Facebook & Instagram Advertising</h2>
+        </div>
+        <div className="cell"><img src="https://my-codal.ir/assets/home-main.webp" alt="imagem" />
+        </div>
+        <div id="smallText">
+          <p>Facebook Premier Level Partner Agency</p>
+          </div>  
+      </div>
+        <div className="navRedirect">
+          <a className="wwuPrincipal" href="">WORK WITH US</a>
+        </div>
+        
     </>
-    );
+  );
 }
