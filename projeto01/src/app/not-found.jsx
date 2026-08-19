98b5514166278ac0d79e97b1f69b83d666c0660e
onlyzs2 <nic123hello@gmail.com>
2026-08-19 14:29:19 -0300

    a

---
+++ projeto01/src/app/not-found.jsx
@@ -0,0 +1,14 @@
+export default function NotFound(){
+
+    return(
+        <>
+            <div className="container-notfound">
+            <div className="flex-notfound">
+                <h2> ERRO 404</h2>
+                <p>pagina nao encontrada ou nao existe</p>
+            </div>
+            </div>
+        </>
+
+)
+}
